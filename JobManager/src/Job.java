d0ab92ea74c54fd4b1c5aa2c1ce5f10a9c338e63
MdRafsanZaman <mrz281@nyu.edu>
2025-12-17 21:33:14 -0500

    Job class changed, need to review design patterns before continuing

--- JobManager/src/Job.java
+++ JobManager/src/Job.java
@@ -8,8 +8,20 @@ public class Job {
     String deadline;
     String dateFound;
     String location;
+
+    public static final String PART = "part"; 
+    
+    private enum time{
+
+        PART,
+        FULL,
+        TEMP,
+        CONTRACT
+
+    }
+
+
     String mode;
-    String time;
 
     ArrayList<Skill> skills;
 
@@ -22,10 +34,12 @@ public class Job {
 
         result += "This job is from " + this.company + " called " + this.name + ", a ";
 
-        if(time.equals("part")) result += "part-time position at ";
-        else if(time.equals("full")) result += "full-time position at ";
-        else if (time.equals("temp")) result += "temporary position at ";
+        // if(time.equals("part")) result += "part-time position at ";
+        // else if(time.equals("full")) result += "full-time position at ";
+        // else if (time.equals("temp")) result += "temporary position at ";
 
+        
+        
         return result;
 
     }
